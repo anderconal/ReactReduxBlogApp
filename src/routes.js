0ed1c2459a5264a4ff2164702cf4508cc1d79f25
Ander Conal <ander.conal@jinnapp.com>
2017-02-09 14:34:45 +0100

    Update with some new routes and the form

--- src/routes.js
+++ src/routes.js
@@ -3,17 +3,11 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 import PostsIndex from './components/posts_index';
-
-// Just an example to use in our routes
-const Greeting = () => {
-    return <div>Ander Conal</div>;
-}
+import PostsNew from './components/posts_new';
 
 export default(
     <Route path="/" component={App}>
         <IndexRoute component={PostsIndex} />
-        <Route path="greet" component={Greeting} />
-        <Route path="greet2" component={Greeting} />
-        <Route path="greet3" component={Greeting} />
+        <Route path="/posts/new" component={PostsNew} />
     </Route>
 );
